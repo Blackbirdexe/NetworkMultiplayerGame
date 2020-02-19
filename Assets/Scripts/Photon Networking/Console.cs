3d07d89b8e19e7d44cc01521bb01bd9c0f10e173
Blackbirdexe <49791840+Blackbirdexe@users.noreply.github.com>
2020-02-20 03:07:21 +0800

    added pun2 and some room stuff

---
+++ Assets/Scripts/Photon Networking/Console.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Console : MonoBehaviour
+{
+    Text consoleTextBox;
+
+    static string myLog = "";
+    private string output;
+    private string stack;
+
+    private void OnEnable()
+    {
+        Application.logMessageReceived += Log;
+    }
+
+    private void OnDisable()
+    {
+        Application.logMessageReceived -= Log;
+    }
+
+    private void Start()
+    {
+        consoleTextBox = this.GetComponent<Text>();
+    }
+
+    public void Log(string logString, string stackTrace, LogType type)
+    {
+        output = logString;
+        stack = stackTrace;
+        myLog = output + "\n" + myLog;
+        if (myLog.Length > 5000)
+        {
+            myLog = myLog.Substring(0, 4000);
+        }
+        consoleTextBox.text = myLog;
+    }
+
+}
---
+++ Assets/Scripts/Photon Networking/Matchmaking/ClientLobby.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+
+public class ClientLobby : MonoBehaviourPunCallbacks
+{
+    public static ClientLobby client_Lobby;
+
+    private void Awake()
+    {
+        client_Lobby = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Client connected to server in " + PhotonNetwork.CloudRegion + " at " + DateTime.Now.ToLocalTime());
+    }
+
+    public void Quickplay()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+
+        switch (returnCode)
+        {
+            case 32760:
+                Debug.Log("Code " + returnCode + ": " + message);
+                Debug.Log("Creating room...");
+                CreateRoom();
+                break;
+        }
+
+    }
+
+    public void CreateRoom()
+    {
+        int roomNum = UnityEngine.Random.Range(0, 255);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 4 };
+        PhotonNetwork.CreateRoom("Room "+ roomNum, roomOps);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
